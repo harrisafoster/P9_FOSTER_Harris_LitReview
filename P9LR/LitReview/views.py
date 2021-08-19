e6fdb8bac20aaf1874d1622f2462a7cf25d5b167
harrisafoster <harrisafoster@gmail.com>
2021-08-19 13:58:47 -0700

    Commit initial, testes avec les templates

---
+++ P9LR/LitReview/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'LitReview/dashboard.html')
+
+
+def pieces(request):
+    return render(request, 'LitReview/pieces.html')
+
+
+def profile(request):
+    return render(request, 'LitReview/profile.html')
+# Create your views here.
