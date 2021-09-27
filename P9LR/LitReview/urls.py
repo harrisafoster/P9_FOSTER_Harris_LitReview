c9312dff246b1fb26a3405d050ba712cfa914e4d
harrisafoster <harrisafoster@gmail.com>
2021-09-27 15:20:46 +0200

    Création de la partie admin, et création du formulaire pour créer des utilisateurs

--- P9LR/LitReview/urls.py
+++ P9LR/LitReview/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.home),
     path('profile/', views.profile),
     path('pieces/', views.pieces),
+    url(r'^signup/$', views.signup, name='signup'), '''template does not exist?'''
 ]
